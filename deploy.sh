25f9749570e4240b3742905230cdde43638c7e56
Aaron Lathrop <alathrop78@gmail.com>
2020-05-12 22:09:16 -0600

    deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+#navigate into the build output directory
+cd dist
+
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Aaron-Lathrop/vue-deploy-to-github-pages.git master:gh-pages
+
+cd -
